a12669a945def4b0e77671e838c01378569aacc2
lightyears1998 <lightyears1998@hotmail.com>
2017-11-08 19:50:27 +0800

    修正错误与提交示例

--- cants.h
+++ cants.h
@@ -5,10 +5,10 @@
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define dwn(i, b, a) for (int i = int(b - 1); i >= int(a); i--)
 
-typedef unsigned UINT
-typedef long long LL
-typedef unsigned long long ULL
+typedef unsigned UINT;
+typedef long long LL;
+typedef unsigned long long ULL;
 /**
- * （不推荐使用上面的简写）
+ * （不推荐使用typedef来简写）
  *  
 **/
---
+++ example.sfi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "cants.h"
+using namespace std;
+
+int main()
+{
+    int i;
+    long l;
+    double d;
+    
+    sfi(i);
+    sfl(l);
+    sfd(d);
+
+    cout << i << endl;
+    cout << l << endl;
+    cout << d << endl;
+}
