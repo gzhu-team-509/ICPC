e99e78b2398e79291959b1ba474a4e44498403d2
lightyears1998 <lightyears1998@hotmail.com>
2017-11-10 20:37:18 +0800

    声明不推荐的写法

--- cant/cants.h
+++ cant/cants.h
@@ -5,10 +5,7 @@
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define dwn(i, b, a) for (int i = int(b - 1); i >= int(a); i--)
 
+// 不推荐使用下面的简写
 typedef unsigned UINT;
 typedef long long LL;
 typedef unsigned long long ULL;
-/**
- * （不推荐使用typedef来简写）
- *  
-**/
