efd9cfd1215caea2788abd046fb214f5d4d1b82d
lightyears1998 <lightyears1998@hotmail.com>
2017-11-26 00:04:46 +0800

    修正二叉树映射中数组容量太小的问题

--- stl/example/ZOJ1167.cpp
+++ stl/example/ZOJ1167.cpp
@@ -2,13 +2,22 @@
  * Trees on the Level
  *
  * 节点存在而其父节点不存在的二叉树是不完整的（根节点除外）
+ * 这样的编程风格可能不算很好，因为它在3处地方打印结果，并且容易因为忘记初始化变量而WA。
+ * 要记得在下一次接受数据前初始化。
+ *
+ * 此题的另一个难点是每棵二叉树不多于256个节点，这题的数据给得相当仁慈。
+ * 以我的这种映射方式，别说是256个节点，就算是一个节点(1,LLLLLLLLLLLLLLLLLLLLLLL)。
+ * 也未必能处理好这个数据。网络上的大神都用其他映射方式来做题。
+ *
+ * 此题为我敲响警钟：理解好题意，正确地分配空间。
 **/
 #include <cstring>
 #include <cstdio>
 using namespace std;
 
 bool complete;
-int tree[260], maxNode;
+int tree[14000], maxNode;
+//! tree[300] 试图用300的数组空间装下256个节点是不可能的，会导致Segment Fault
 
 void init()
 {
@@ -24,15 +33,15 @@ int main()
     while (scanf(" %c", &ch) == 1)
     {
         int data = 0;
-        if (scanf("%d", &data) == 1)
+        if (scanf(" %d", &data) == 1)
         {
-            scanf("%*c");
+            scanf(" %*c");
 
 //!         char pos[10]; scanf("%[^)]", pos);
 // 使用局部cstring要注意初始化，尤其是想通过scanf()对它进行初始化时。
-            char pos[10] = "\0"; scanf("%[^)]", pos);
+            char pos[20] = ""; scanf(" %[^)]", pos);
 
-            scanf("%*c");
+            scanf(" %*c");
 
             if (!complete) continue;
 
@@ -48,21 +57,29 @@ int main()
         }
         else
         {
-            scanf("%*c");
+            scanf(" %*c");
             if (complete)
             {
-                for (int i = 2; i <= maxNode; i++)
+                if (maxNode == 0)
+                {
+                    printf("not complete\n");
+                    init();
+                    continue;
+                }
+                for (int i = 1; i <= maxNode; i++)
                     if (tree[i] && !tree[i >> 1]) complete = false;
                 if (complete) {
                     printf("%d", tree[1]);
-                    for (int i = 1; i <= maxNode; i++)
+                    for (int i = 2; i <= maxNode; i++)
                     {
                         if (tree[i]) printf(" %d", tree[i]);
                     }
                     printf("\n");
+                    init();
                 }
                 else {
                     printf("not complete\n");
+                    init();
                 }
             }
             else
