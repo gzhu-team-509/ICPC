4cb6d1978438394fc2ef8c099910a6465e4aa36c
lightyears1998 <lightyears1998@hotmail.com>
2017-12-10 22:41:52 +0800

    根据Capman学长的建议进行修正

--- cant/cants.h
+++ cant/cants.h
@@ -5,7 +5,9 @@
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define dwn(i, b, a) for (int i = int(b - 1); i >= int(a); i--)
 
-// 不推荐使用下面的简写
+// UPD12.10 Capman学长今天特别提示我
+// 上面的宏定义时学长自己的习惯
+// 下面的typedef语句才是对多数人都实用的定义
 typedef unsigned UINT;
 typedef long long LL;
 typedef unsigned long long ULL;
