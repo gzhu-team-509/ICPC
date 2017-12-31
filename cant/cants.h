49ed5d86670daa443189757d796642b126d778d0
lightyears1998 <lightyears1998@hotmail.com>
2017-12-31 12:03:58 +0800

    Add new cants: using LL = long long;
    It works since C++11.

--- cant/cants.h
+++ cant/cants.h
@@ -5,9 +5,13 @@
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define dwn(i, b, a) for (int i = int(b - 1); i >= int(a); i--)
 
-// UPD12.10 Capman学长今天特别提示我
+// UPD12.10 Caproner学长提示我
 // 上面的宏定义是学长自己的习惯
-// 下面的typedef语句才是对多数人都实用的定义
+// 下面的typedef语句是对多数人都实用的定义
 typedef unsigned UINT;
 typedef long long LL;
 typedef unsigned long long ULL;
+
+// 等价形式
+using LL = long long;
+using DB = long double;
