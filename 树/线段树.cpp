e99a2b06a681a0ed3adc6c6d9638ea85f3903efa
lightyears1998 <lightyears1998@hotmail.com>
2018-01-25 21:03:24 +0800

    Update a new SegmentTree Feature: Range Add/Sub

--- 树/线段树.cpp
+++ 树/线段树.cpp
@@ -9,6 +9,7 @@ int leaf[maxN];
 struct SegmentTreeNode{
 	int l, r;
 	long long val, vmx, vmn;
+	long long lazy;
 } node[maxN<<2]; 
 
 void init()
@@ -20,6 +21,7 @@ void init()
 void build(int rt, int l, int r, int *arr)
 {
 	node[rt].l = l, node[rt].r = r;
+	node[rt].lazy = 0;
 	if (l==r)
 	{
 		leaf[l] = rt;
@@ -36,7 +38,7 @@ void build(int rt, int l, int r, int *arr)
 	}
 }
 
-void push(int rt)
+void pushup(int rt)
 {
 	while (rt>>=1) {
 		node[rt].val = node[rt<<1].val + node[rt<<1|1].val;
@@ -44,24 +46,56 @@ void push(int rt)
 		node[rt].vmn = min(node[rt<<1].vmn, node[rt<<1|1].vmn);
 	}
 }
+ 
+void pushdown(int rt)
+{
+	if (node[rt].lazy) {
+		node[rt].val += node[rt].lazy*(node[rt].r-node[rt].l+1);
+		node[rt].vmx += node[rt].lazy;
+		node[rt].vmn += node[rt].lazy;
+		node[rt<<1].lazy += node[rt].lazy;
+		node[rt<<1|1].lazy += node[rt].lazy;
+	}
+	node[rt].lazy = 0;
+}
 
 void modify(int rt, int val) 
 {
 	node[rt].val = node[rt].vmx = node[rt].vmn = val; 
-	push(rt); 
+	pushup(rt); 
 }
 
 void update(int rt, int diff)
 {
 	node[rt].val += diff;
 	node[rt].vmx = node[rt].vmn = node[rt].val; 
-	push(rt);
+	pushup(rt);
+}
+
+void update(int rt, int l, int r, int diff)
+{
+	if (l<=node[rt].l&&node[rt].r<=r) {
+		node[rt].lazy += diff;
+		return;
+	}
+	int mid = (node[rt].l+node[rt].r)/2;
+	if (r<=mid) {
+		update(rt<<1, l, r, diff);
+	} 
+	else if (mid<l) {
+		update(rt<<1|1, l, r, diff);
+	}
+	else {
+		update(rt<<1, l, r, diff);
+		update(rt<<1|1, l, r, diff);
+	}
 }
 
 long long query(int rt, int l, int r)
 {
-	if (node[rt].l==l&&node[rt].r==r) 
-		return node[rt].val;
+	if (node[rt].l==l&&node[rt].r==r)
+		return node[rt].val + node[rt].lazy;
+	pushdown(rt);
 	int mid = (node[rt].l+node[rt].r)/2;
 	if (r<=mid)
 		return query(rt<<1, l, r);
@@ -74,7 +108,8 @@ long long query(int rt, int l, int r)
 long long query_max(int rt, int l, int r)
 {
 	if (node[rt].l==l&&node[rt].r==r) 
-		return node[rt].vmx;
+		return node[rt].vmx + node[rt].lazy;
+	pushdown(rt);
 	int mid = (node[rt].l+node[rt].r)/2;
 	if (r<=mid)
 		return query_max(rt<<1, l, r);
@@ -86,8 +121,9 @@ long long query_max(int rt, int l, int r)
 
 long long query_min(int rt, int l, int r)
 {
-	if (node[rt].l==l&&node[rt].r==r) 
-		return node[rt].vmn;
+	if (node[rt].l==l&&node[rt].r==r)
+		return node[rt].vmn + node[rt].lazy;
+	pushdown(rt);
 	int mid = (node[rt].l+node[rt].r)/2;
 	if (r<=mid)
 		return query_min(rt<<1, l, r);
@@ -95,4 +131,30 @@ long long query_min(int rt, int l, int r)
 		return query_min(rt<<1|1, l, r);
 	else
 		return min(query_min(rt<<1, l, mid), query_min(rt<<1|1, mid+1, r));
+}
+
+int main()
+{
+	#ifdef LOCAL
+	freopen("in.txt", "r", stdin);
+	#endif
+	
+	int n;
+	while (scanf("%d", &n) == 1 && n)
+	{
+		for (int i = 1; i <= n; ++i)
+			arr[i] = 0;
+		build(1, 1, n, arr);
+		for (int i = 1; i <= n; ++i)
+		{
+			int a, b; scanf("%d%d", &a, &b);
+			update(1, a, b, 1);
+		}
+		
+		for (int i = 1; i <= n; ++i)
+		{
+			printf("%lld", query(1, i, i));
+			putchar(i != n ? ' ' : '\n'); 
+		}
+	}
 }
