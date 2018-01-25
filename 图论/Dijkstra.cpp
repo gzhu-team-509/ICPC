c7efae75980b9f5fd271cd9e9d6737ea9d7a4576
lightyears <lightyears1998@hotmail.com>
2018-01-25 22:31:22 +0800

    Update Dijkstra.cpp

--- 图论/Dijkstra.cpp
+++ 图论/Dijkstra.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+// Dijkstra - 无负权边图单源最短路
+// 输入起点，终点，边的权重，运行算法
+// 算法执行的结果存放在dis[t]中
 const int INF = 0x3F3F3F3F;
 int n, m, s, t;
 int path[220][220];
@@ -28,29 +28,3 @@ void dijkstra()
 			dis[j] = min(dis[j], dis[marker] + path[marker][j]);
 	}
 }
-
-int main()
-{
-	#ifdef LOCAL
-	freopen("in.txt", "r", stdin);
-	#endif
-	
-	while (scanf("%d%d", &n, &m) == 2)
-	{
-		memset(path, 0x3F, sizeof(path));
-		for (int i = 0; i < n; ++i)
-			path[i][i] = 0;
-		
-		while (m--)
-		{
-			int a, b, x; scanf("%d%d%d", &a, &b, &x);
-			path[a][b] = min(path[a][b], x);
-			path[b][a] = min(path[b][a], x);
-		}
-		scanf("%d%d", &s, &t);
-		
-		dijkstra();
-		dis[t] == INF ? printf("-1\n") : printf("%d\n", dis[t]);
-	}
-}
-
