0e041bf1abcd22c4df058dc0b95f602c51dab1f1
lightyears1998 <lightyears1998@hotmail.com>
2018-02-22 12:31:39 +0800

    装入一只启发式优化的并查集变体

---
+++ 箱子/变体/并查集启发式合并LibreOJ109.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+
+int fa[4000004], w[4000004];
+
+void init(int n)
+{
+	for (int i = 0; i < n; ++i)	fa[i] = i;
+}
+
+int find(int x)
+{
+	return fa[x] == x ? x : fa[x] = find(fa[x]);
+}
+
+bool query(int a, int b)
+{
+	return find(a) == find(b);
+}
+
+void merge(int a, int b)
+{
+	a = find(a), b = find(b);
+	if (a == b) return;
+	if (w[a] > w[b]) swap(a, b);
+	fa[a] = b, ++w[b];
+}
+
+int main()
+{
+	int n, m; scanf("%d%d", &n, &m);
+	init(n); LL ans = 0;
+	
+	while (m--)
+	{
+		int op, u, v; scanf("%d%d%d", &op, &u, &v);
+		switch (op) 
+		{
+			case 0: {
+				merge(u, v);
+				break;
+			}
+			case 1: {
+				ans <<= 1;
+				if (query(u, v)) ans += 1;
+				ans %= 998244353;
+				break;
+			}
+		}
+	}
+	printf("%lld\n", ans);
+}
